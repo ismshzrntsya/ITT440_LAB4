4ecd539aace52e779fddb526122ae0c133b2336b
ismshzrntsya <ismshzrn.ntsya@gmail.com>
2021-05-07 15:31:01 +0000

    commit for 4.1

--- 4.1a.c
+++ 4.1a.c
@@ -11,7 +11,7 @@ int main(int argc, char*argv[])
 
 
 	//Create socket
-	socket_desc =socket(AF_INET,SOCK_STREAM,0);
+	socket_desc =socket(AF_INET,SOCK_DGRAM,0);
 	if (socket_desc == -1)
 	{
 		printf("Could not create socket");
